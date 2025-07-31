0b4a8f082478ef2b27791ab4a360bb4b525ac589
Shreya Yadav <shreyadavagr42@gmail.com>
2025-07-31 23:22:09 +0530

    Q-3 DONEgit add .

---
+++ Practice set/CH-1/Q-3.py
@@ -0,0 +1,18 @@
+#  write a python program to print the content of a directory using the OS module. search online for the function which does that.
+ 
+
+import os
+
+# Specify the directory path (you can change this)
+path = "/"
+
+# List all files and directories in the given path
+try:
+    contents = os.listdir(path)
+    print(f"Contents of directory '{path}':")
+    for item in contents:
+        print(item)
+except FileNotFoundError:
+    print(f"The directory '{path}' does not exist.")
+except PermissionError:
+    print(f"No permission to access '{path}'.")
