734b3f4a6656fddbce58c138d8d2b9aa10a91a63
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-01 03:19:42 +0530

    Q-5 DONEgit add .

---
+++ Practice set/CH-2/Q-5.py
@@ -0,0 +1,6 @@
+# write a python program to find an average of two numbers entered by user .
+
+num1= int(input("enter the first number:"))
+num2 = int(input("enter the second number:"))
+average = (num1 + num2) / 2
+print("the average of a two numbers is:", average)
