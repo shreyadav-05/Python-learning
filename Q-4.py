f830a6a0bc2eb3e1bc6e3c4f24712c47a4437c61
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-01 02:16:09 +0530

    Q-4 donegit add .

---
+++ Q-4.py
@@ -0,0 +1,6 @@
+# use comperision operator to find out weather a given variable a greater than 'b' or not.Take a=34 and b=80.
+
+a = int(input("enter number 1,"))
+b = int(input("enter number 2,"))
+
+print("enter than b is ",a>b)
