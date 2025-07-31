6c7c2513daec91f7940955e50ea277c8f38782fd
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-01 01:58:26 +0530

    Q-2 completegit add .

---
+++ Practice set/CH-2/Q-2.py
@@ -0,0 +1,7 @@
+# write a python program to find remainder when is devided by z 
+
+number = int(input("enter the number:"))
+z = int(input("divided by z:"))
+
+remainder = number/z
+print("remainder is after divided by z:" ,remainder)
