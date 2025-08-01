d6cac474fc9c626bd7c612735c0c01a09030765a
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-02 01:59:04 +0530

    Q-2 complete

--- CH-3/Q-2.py
+++ CH-3/Q-2.py
@@ -1,1 +1,8 @@
-#
+# write a program to fill in a letter template given below with name and date 
+letter = '''
+        Dear <|Name|>,
+        you are selected!
+        <|Date|>
+          '''
+
+print(letter.replace("<|Name|>","Shreya").replace("<|Date|>","05 october 2005"))
