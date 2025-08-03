52d322cd2035677e52fca54704f33ba89c34725b
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-04 00:00:05 +0530

    Q-2 DONEgit add .

---
+++ Practice set/CH-5/Q-2.py
@@ -0,0 +1,31 @@
+#
+s = set()
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+n = input("enter number :")
+s.add(int(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
