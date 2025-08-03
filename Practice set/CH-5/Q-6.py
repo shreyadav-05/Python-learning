e2d18d44975df0912859f25cf15f6849236e5e89
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-04 00:55:29 +0530

    Q-6 DONEgit add .

---
+++ Practice set/CH-5/Q-6.py
@@ -0,0 +1,24 @@
+ # Create an empty  dictionary. Allow 4 friends to enter their favourite language as value and use thier name as key.
+
+d={}
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+print(d)
