33df7d68797f6eed26da442a7cac5f122a26e4f5
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-05 23:01:03 +0530

    Q-1 DONEgit add .

---
+++ Concept/CH-6/Q-1.py
@@ -0,0 +1,46 @@
+#  # Write a program to find the greatest of four numbers entred by the user!
+"""Q-1"""
+A=int(input("Enter number:"))
+B=int(input("Enter number:"))
+C=int(input("Enter number:"))
+D=int(input("Enter number:"))
+
+if(A>B and A>C and A>D):
+  print("A is the greatesT number:",A)
+
+elif(B>A and B>C and B>D):
+  print("B is the greatesT number:",B)
+
+elif(C>A and C>B and C>D):
+  print("C is the greatesT number:",C)
+
+elif(D>B and D>C and D>A):
+  print("D is the greatesT number:",D)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
