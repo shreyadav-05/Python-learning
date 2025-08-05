e74ab79427f3c0fb30944dcf7965bee0f00ea6cd
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-05 23:16:54 +0530

    Q-4 DONEgit add .

---
+++ Concept/CH-6/Q-4.py
@@ -0,0 +1,22 @@
+#write a program to find whether a given username contains less than 10 characters or not
+
+
+Username=input("Create your username:")
+
+if(len(Username)<10):
+  print("It should atleast of 10 characters!!")
+
+else:
+  print("valid username!!")
+
+
+#Write a program which finds out whether a given name is present or not 
+
+n=input("Enter the name: ")
+st="Shreya "
+
+if(st==n):
+  print("yes, given name is present")
+
+else:
+  print("no, given name is not present ")
