18751f2ecc09de281abd15d48532193a5b46fc78
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-08 23:19:15 +0530

    Q-13 DONEgit add .

--- Practice set/CH-7/Q-1.py
+++ Practice set/CH-7/Q-1.py
@@ -1,1 +1,5 @@
-EK kya kr r
+# print numbers from 1 to 10.
+i = 1
+while (i<11):
+    print(i)
+    i+=1
--- Practice set/CH-7/Q-13.py
+++ Practice set/CH-7/Q-13.py
@@ -1,1 +1,6 @@
-# 
+#  Attempt problem 1 using while loop.
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
