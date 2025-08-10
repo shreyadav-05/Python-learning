e26f2c264edcd38bdcf4a8139eafa2ec82ee7e6b
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-10 23:27:11 +0530

    Create CH-7.2 FOR loops.py

---
+++ Concept/CH-7.2 FOR loops.py
@@ -0,0 +1,15 @@
+#For loops = execute a block of code a fixed number of times.
+# you can iterate over a range, string, sequence, etc
+
+# for x in range(1 ,11,2) :
+#   print(x)
+# print("happy new year!")
+
+
+
+for x in range(1 ,21) :
+  if x ==13:
+    # continue       # skips the iteration
+    break            # once we reached to required condition, we escape tje
+  else:
+    print(x)
