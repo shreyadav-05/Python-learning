7a7d0802636acc5c1b4e50cfc8d911ecb6df2192
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-11 23:25:15 +0530

    Add files via upload

---
+++ Concept/CH-7.5 ELSE_FOR_LOOPS.py
@@ -0,0 +1,39 @@
+# we can use the ELSE in FOR LOOP ,
+# it is use when the LOOP get exhaust[ends when the it can't go further]  
+l=[1,7,5]
+for a in l:      
+  print(a) 
+
+else:
+  print("bhai mere itna hi hai list me")
+
+print("\n")
+
+#LOOPS thorugh  string 
+string="ABHI YADAV"
+for i in string[:4]:    # it is done by the string slicing method 
+   print(i)
+
+else:
+  print("bss mera naam hi kaafi hai 😏")
+
+
+
+ # Loop through a dictionary
+d = {"a": 1, "b": 2}
+for key in d:
+    print(key, d[key])  
+
+else:
+   print("le ho gaya")    
+
+
+
+for i in range(0,20,5):  #t will generate a sequence starting from 0 up to (but not including, n-1) 20, with a step size of 5.
+  print(i) 
+
+else:
+   print("DONE")
+
+
+ 
