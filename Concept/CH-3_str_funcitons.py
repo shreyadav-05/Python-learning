3e3f15c5a7c18a588330668aa1081693b1e0ae40
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-11 23:32:11 +0530

    String functions done

---
+++ Concept/CH-3_str_funcitons.py
@@ -0,0 +1,9 @@
+Name = "Abhi yadav"
+
+#These are some funciton of strings.
+
+print(len(Name))            #Length
+print(Name.endswith("v")) #shows whether the tru or false
+print(Name.startswith("ha")) #shows whether the tru or false
+print(Name.capitalize())     #it make the letter capital and uncapitalize if there is any after the capital letter 
+
