5feaca7ce5fc03ee46a72c413b1bfb71f8a467b7
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-12 22:26:56 +0530

    Create Q-7.py

---
+++ Practice set/CH-5/Q-7.py
@@ -0,0 +1,27 @@
+#Write a program to create an dicitionary of hindi words with the values of English transaltion 
+"""Q-1
+DIC ={ 
+  "pustak" : "book",  
+  "paani" : "water", 
+  "aadmi" : "man  ",
+  "aurat" : "woman" , 
+  "ghar" : "house"  ,
+  "billi" : "cat  ",
+  "kutta" : "dog  ",
+  "sooraj" : "sun  ",
+  "chaand" : "moon " ,
+  "ped" : "tree " ,
+  "phal" : "fruit" , 
+  "doodh" : "milk " ,
+  "khidki" : "window" , 
+  "darwaza" : "door ", 
+  "school" : "school" , 
+  "khel" : "game " ,
+  "dost" : "friend"  ,
+ "khushi" : "happiness" , 
+  "dukh" : "sorrow"  ,
+  "prem" : "love"}
+
+wORD=input("Enter the word:")
+print(DIC[wORD])
+"""
