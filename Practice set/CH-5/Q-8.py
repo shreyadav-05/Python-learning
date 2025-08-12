96c8c85ae7b457f4c819e91c0ee1aaed95ce7030
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-12 22:27:22 +0530

    Create Q-8.py

---
+++ Practice set/CH-5/Q-8.py
@@ -0,0 +1,29 @@
+#Write a program to input 8 unique numbers from the users and display    at once  
+""" Q-2
+s=set()
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+n=input("Enter the first number")
+s.add(int(n))
+
+print(s)
+"""
