64b489094eb7f1097004c7367a6c2225bdac5400
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-12 22:24:27 +0530

    Pip, modules, comments - concept understood

---
+++ Concept/CH-1 pip,module, comment.py
@@ -0,0 +1,24 @@
+#                   Python
+#                 ----------
+#  Python is a simple and easy to understand language which feels like reading simple
+# English. This Pseudo code nature is easy to learn and understandable by beginners.
+
+
+#             MODULES
+#  A module is a file containing code written by somebody else (usually) which can be imported and used in our programs.
+
+#       Types of MOdules
+"""
+1. Built in Module (pre-installed)
+      ex- os, random etc.
+2. External Modules ( Need to install using pip).
+      ex- tensorflow, flusk etc.
+
+"""
+
+#           PiP
+#  Pip is the package manager for python. You can use a pip to install a module on your system.
+
+
+
+print("Hello World!!")
