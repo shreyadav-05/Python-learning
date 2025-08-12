a8d12685a644dee337bd759bc6d4a3c98ab40bfa
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-12 22:29:02 +0530

    Create Q-10.py

---
+++ Practice set/CH-5/Q-10.py
@@ -0,0 +1,25 @@
+# Create an empty  dictionary. Allow 4 friends to enter their favourite language as value and use thier name as key.
+"""Q-6"""
+d={}
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+
+F=input("Enter Friends's name:")
+L=input("Enter the lanugage name:")
+
+d.update({F:L})
+print(d)
+#
