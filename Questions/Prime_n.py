d245ec86b2fedfb7dfafe70bd95ebc70a25d2727
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-17 23:11:18 +0530

    Prime numbers

--- Questions/Prime_n.py
+++ Questions/Prime_n.py
@@ -1,11 +1,9 @@
-# mujhe nhi a raha yeh m chatgpt se punch rhii
+# 
 
-print("Prime num from 1 to n " )
-num = int(input("enter n:"))
-for i in range (2 , num):               
-        if (num % i)  == 0:      
-           print(f"{num} is not prime")     
-           break
-        else:
-         print(f"{num} is  a prime number")
+num = int(input("enter a num: "))
+for i in range (2 , num):
+    if num % i ==0:  
+        print(num , "is not a prime num")
         break
+    else:
+        print(num, "is a prime num")
