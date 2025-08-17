06ee6859efebc3dbd959449e127d56105899a9d9
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-18 02:18:41 +0530

    pattern

--- Questions/pattern2.py
+++ Questions/pattern2.py
@@ -1,18 +1,5 @@
-"""
-1
-12
-123
-1234
-12345
-
-
-"""
-
-r = 5
-for i in range(1 , r+1):
+r = 5 
+for i in range(1,r+1):
     for j in range(1 , i+1):
-        print(j ,end = " ")
-
+        print(j , end=" ")
     print()
-    
-
