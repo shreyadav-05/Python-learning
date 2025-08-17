f2837f913a27335db9e7f4b34da6f816f4d653a9
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-18 04:15:26 +0530

    QUESTION DONE

--- Questions/pattern2.py
+++ Questions/pattern2.py
@@ -1,5 +1,5 @@
-r = 5 
-for i in range(1,r+1):
-    for j in range(1 , i+1):
+r = 5  
+for i in range(1,r+1):        # 
+    for j in range(1 , i+1):        # yeh loop kha se kha tak chalega -> byee so raha me 
         print(j , end=" ")
     print()
