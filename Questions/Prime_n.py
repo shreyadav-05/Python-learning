4d58cdb1778acb0cf3f5d299ddafd5dc4d0ff3e0
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-18 00:20:47 +0530

    QUESTION DONE

--- Questions/Prime_n.py
+++ Questions/Prime_n.py
@@ -1,18 +1,13 @@
-# 
-
-
-num = int(input("enter a num: "))
+num = int(input("enter num:"))
 if num == 1:
-    print("1 not a prime Number nor Composite") 
-elif num ==2:
-  print("2 is a prime number")
-elif num>1:
-    for i in range (2 , num):    
-     if num % i ==0:  
-        print(num , "is not a prime num")
-        break
-    
-    else:
-        print(num, "is a prime num")
-
-
+    print("1 is not a prime number nor composite")
+elif num == 2:
+    print("2 is prime number")
+elif num > 1:
+    for i in range(2 , num):
+        if num % i == 0:
+            print(num, "is not prime number")
+            break
+        else:
+            print(num," is a prime number")
+            
