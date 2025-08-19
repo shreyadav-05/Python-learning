6063b94f8a843a1b026e0628571fa57810a3e9fe
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-19 23:55:37 +0530

    Create username.py

---
+++ Questions/username.py
@@ -0,0 +1,23 @@
+#write a program to find whether a given username contains less than 10 characters or not
+
+"""Q-4 """
+
+Username=input("Create your username:")
+
+if(len(Username)<10):
+  print("It should atleast of 10 characters!!")
+
+else:
+  print("valid username!!")
+
+
+#Write a program which finds out whether a given name is present or not 
+
+n=input("Enter the name: ")
+st="Abhi "
+
+if(st==n):
+  print("yes, given name is present")
+
+else:
+  print("no, given name is not present ")
