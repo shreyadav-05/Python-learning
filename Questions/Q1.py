f6181e2daf80ce0cde3b5a7e8233130335bddaee
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-19 23:52:42 +0530

    Create Q1.py

---
+++ Questions/Q1.py
@@ -0,0 +1,23 @@
+# Print first 10 natural numbers using while loop
+
+i=1
+while(i<11):
+  print(i)
+  i+=1
+
+
+#Print the following pattern 
+"""
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+"""
+n=int(input("Enter: "))
+
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print(j,end='')
+  
+  print("")
