5fd6377cbf32f056e53c9a5b183f41cc836578d0
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-21 23:37:46 +0530

    Q-9DONE

---
+++ Questions/Q-10.py
@@ -0,0 +1,7 @@
+#Reverse a number
+num = 1234
+rev = 0
+while num > 0:
+    rev = rev * 10 + num % 10
+    num //= 10
+print("Reverse =", rev)
---
+++ Questions/Q-9.py
@@ -0,0 +1,5 @@
+#Print only vowels from a string
+text = "hello world"
+for ch in text:
+    if ch in "aeiou":
+        print(ch)
