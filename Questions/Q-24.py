26f35c576dce0fb8ff2753c0834028193efd2870
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-23 21:42:03 +0530

    Q-25 done

--- Questions/Q-24.py
+++ Questions/Q-24.py
@@ -5,3 +5,5 @@ if num % 2 == 0:
     print("Even number")
 else:
     print("Odd number")
+
+apka rock paer kha hai
---
+++ Questions/Q-25.py
@@ -0,0 +1,4 @@
+# Sum of Numbers in a List
+numbers = [2, 4, 6, 8]
+total = sum(numbers)
+print("Sum:", total)
