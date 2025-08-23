a534d8f54b71bb845a5b70025c0b320582261372
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-24 00:26:08 +0530

    Q-37 done

---
+++ Questions/Q37.py
@@ -0,0 +1,6 @@
+#Find the Smallest of Three Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Smallest:", min(a, b, c))
