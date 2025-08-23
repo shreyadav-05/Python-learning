cd44daedc69252df8d7888a8a2236f7870581175
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-23 22:26:33 +0530

    Q-33 done

---
+++ Questions/Q-33.py
@@ -0,0 +1,8 @@
+#Find the Largest of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest:", a)
+else:
+    print("Largest:", b)
