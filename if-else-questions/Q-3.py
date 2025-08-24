c85e425603161fc3b607ec45c0cefbd10833c307
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-24 22:31:32 +0530

    Q-3 DONE

---
+++ if-else-questions/Q-3.py
@@ -0,0 +1,6 @@
+#Check whether a person is eligible to vote (age ≥ 18).
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
