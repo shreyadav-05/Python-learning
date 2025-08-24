6821f2a5177fe00550a6518b1ded2c2fba10fc87
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-24 22:39:05 +0530

    Q-5 DONE

---
+++ if-else-questions/Q-5.py
@@ -0,0 +1,22 @@
+#Write a program that assigns grades based on marks.marks = int(input("Enter your marks: ")  
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+"""
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+    """
