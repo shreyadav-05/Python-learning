49d4754f7ab5c45eadc562efa7196fcacf5b7b90
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-24 22:28:33 +0530

    Q-2done

---
+++ if-else-questions/Q-2.py
@@ -0,0 +1,10 @@
+#Positive, Negative, or Zero
+#Write a program that takes a number as input and checks whether it is positive, negative, or zero.
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
