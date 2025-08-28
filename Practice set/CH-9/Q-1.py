8e115890a762418cc2b2e1344ae5169362d0541e
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-28 23:57:26 +0530

    Q-1 DONE

---
+++ Practice set/CH-9/Q-1.py
@@ -0,0 +1,5 @@
+# Write a program to create a file myfile.txt and write "Hello, Python!" 
+
+with open("myfile.txt", "w") as f:
+    f.write("Hello, Python!")
+print("File created and written successfully.")
