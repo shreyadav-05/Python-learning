321afaba276337f63a9dc02550e148d0179f02c5
Shreya Yadav <shreyadavagr42@gmail.com>
2025-08-30 00:55:22 +0530

    Q-4 done

--- Practice set/CH-9/Q-4.py
+++ Practice set/CH-9/Q-4.py
@@ -1,9 +1,6 @@
-#Find "python" in a log file
-# Problem 6
-with open("log.txt", "r") as f:
-    content = f.read()
+#Read the content of a file
 
-if "python" in content.lower():
-    print("Yes, 'python' is present in the log file.")
-else:
-    print("No, 'python' is not present in the log file.")
+with open("myfile.txt", "r") as f:
+    content = f.read()
+print("File Content:\n", content)
+
