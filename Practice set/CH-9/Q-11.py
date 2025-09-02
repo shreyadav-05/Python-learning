1db0ca8f65dd9fc958e6b20a732e856708406412
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-03 00:24:04 +0530

    Q-11 DONE

---
+++ Practice set/CH-9/Q-11.py
@@ -0,0 +1,8 @@
+#Repeat program 4 for a list of such words to be censored.
+word = ["Shreya","Pooja","Anusatha"]
+with open("file.txt","r")as f:
+    content = f.read()
+for word in word:
+    content = content.replace(word,"#" * len(word))
+    with open("file.txt","w")as f:
+        f.write(content)
