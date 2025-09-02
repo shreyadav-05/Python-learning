2d4fadad1d4015677ba84926fd76531866abdef0
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-03 00:14:02 +0530

    Q-10 DONE

--- Practice set/CH-9/Q-10.py
+++ Practice set/CH-9/Q-10.py
@@ -1,1 +1,7 @@
 #A file contains a word Donkey multiple times. You need to write a program which replaces this word with ###### by updating the same file.
+word = "Shreya"
+with open("file.txt","r")as f:
+    content = f.read()
+    contentNew = content.replace(word,"######")
+    with open("file.txt","w")as f:
+        f.write(contentNew)
