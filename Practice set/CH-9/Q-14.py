dcebb2bc706e649c6429a241c924d36f5f0969c9
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-03 23:45:44 +0530

    Q-14 done

---
+++ Practice set/CH-9/Q-14.py
@@ -0,0 +1,11 @@
+#Write a program to find out whether a file is identical & matches the content of another file.
+with open("file.txt") as f:
+    content1 = f.read()   # use content1 instead of contant
+
+with open("poem.txt") as f:
+    content2 = f.read()
+
+if content1 == content2:
+    print("Yes, these files are identical")
+else:
+    print("No, these files are not identical")
