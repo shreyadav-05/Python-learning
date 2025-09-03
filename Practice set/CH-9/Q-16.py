0b67be16cc04101614389864fdff3b0d40edd475
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-04 00:07:13 +0530

    Q-16 done

---
+++ Practice set/CH-9/Q-16.py
@@ -0,0 +1,6 @@
+#Write a python program to rename a file to renamed_by_python.txt.
+with open("old.txt")as f:
+    content = f.read()
+
+    with open("renamed_by_txt","w") as f:
+        f.write(content)
