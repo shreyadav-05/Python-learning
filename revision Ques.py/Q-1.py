8a0a4efa9e301360991d9a141521302d30496c09
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-05 20:10:56 +0530

    Maximum number in list

--- revision Ques.py/Q-1.py
+++ revision Ques.py/Q-1.py
@@ -7,3 +7,14 @@ for ch in string:
     if ch in vowels :
         count +=1
 print ("Total vowels :",count)
+
+
+
+# #find the maximum element in a list 
+numbers = [55,23,90,58,68]
+max_num = numbers [0]
+for num in numbers:
+    if num > max_num:
+        max_num = num
+        print("maximum element:",max_num)
+
