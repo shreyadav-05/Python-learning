2acd9277d1a46eb66584c25d17905d85bc029d70
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-05 20:32:32 +0530

    prime number

--- Questions/Prime__n.py
+++ Questions/Prime__n.py
@@ -3,6 +3,7 @@ if num == 1:
     print("1 is not a prime number nor composite")
 elif num == 2:
     print("2 is prime number")
+    
 elif num > 1:
     for i in range(2 , num):
         if num % i == 0:
--- revision Ques.py/Q-1.py
+++ revision Ques.py/Q-1.py
@@ -18,3 +18,13 @@ for num in numbers:
         max_num = num
         print("maximum element:",max_num)
 
+#Check prime number
+num = int(input("enter the num"))
+if num > 1:
+    for i in range(2,num):
+        if num % i == 0:
+            print(num, "is not prime number")
+            break
+        else:
+            print(num," is a prime number")
+            
