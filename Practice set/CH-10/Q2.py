807fa7f50bce8565a67964b22039d14780e155fc
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-07 23:57:43 +0530

    Q2 done

---
+++ Practice set/CH-10/Q2.py
@@ -0,0 +1,21 @@
+# Add a static method in problem 2, to greet the user with hello 
+"""Q-4"""
+class calculator:
+  def __init__(self,n):
+    self.n=n
+
+  def square(self):
+    print(f"The square is {self.n*self.n}")
+  def cube(self):
+    print(f"The cube is {self.n*self.n*self.n}")
+  def sqaureroot(self):
+    print(f"The square root is {self.n**1/2}")
+  @staticmethod
+  def greet():
+    print("Hello, good morning😊 ")
+
+a=calculator(4)
+a.greet()
+a.square()
+a.sqaureroot()
+a.cube()
