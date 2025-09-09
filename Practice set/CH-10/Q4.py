599630e6ebefba67b92bfa6146cccd3f4c8f7dde
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-09 23:52:12 +0530

    Q4 DONE

--- Practice set/CH-10/Q4.py
+++ Practice set/CH-10/Q4.py
@@ -5,7 +5,15 @@ class Calculator:
 
     def square(self):
         print(f"The square is {self.n * self.n}")
+    def cube(self):
+        print(f"The cube is {self.n * self.n}")
+    def squareroot(self):
+        print(f"The squareroot is {self.n * self.n}")
+
+
+    
 
 a = Calculator(7)
 a.square()
-
+a.cube()
+a.squareroot()
---
+++ Practice set/CH-10/Q5.py
@@ -0,0 +1,1 @@
+#Create a class with a class attribute a; create an object from it and set a directly using object.a = 0. Does this change the class attribute?
