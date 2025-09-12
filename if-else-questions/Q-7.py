34f83a09286fae5fd782b422ba32dac06e3e0671
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-12 23:12:09 +0530

    done

--- if-else-questions/Q-7.py
+++ if-else-questions/Q-7.py
@@ -1,4 +1,5 @@
 #Check if a character is a vowel
+
 ch = 'e'
 if ch in 'dtrerteuyy bvfiugfhg hfgjfkg':
     print("Vowel")
--- if-else-questions/Q-8.py
+++ if-else-questions/Q-8.py
@@ -1,7 +1,13 @@
 #Find the smallest of two numbers
+
+
 a = 15
 b = 20
 if a < b:
     print("a is smaller")
 else:
     print("b is smaller")
+
+
+
+
