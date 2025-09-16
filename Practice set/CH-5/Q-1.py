5cc757539843c9eb6d312ad719a6a46291a02631
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-16 23:12:05 +0530

    Q done

--- Practice set/CH-5/Q-1.py
+++ Practice set/CH-5/Q-1.py
@@ -1,5 +1,7 @@
 # Write a program to create an dicitionary of hindi words with the values of English transaltion .
 
+
+
 words = {
     "water" : "pani",
     "dog"  : "kutta",
@@ -7,3 +9,4 @@ words = {
 }
 word = input("enter the word you want meaning of:")
 print(words[word])
+
