cf4260f446ea800080b1ae4792f75a686052d885
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-16 23:15:09 +0530

    Q done

--- Practice set/CH-5/Q-2.py
+++ Practice set/CH-5/Q-2.py
@@ -1,5 +1,7 @@
 #Write a program to input 8 unique numbers from the users and display    at once  
 
+
+
 s = set()
 n = input("enter number :")
 s.add(int(n))
@@ -18,6 +20,8 @@ s.add(int(n))
 n = input("enter number :")
 s.add(int(n))
 print(s)
+
+
 
 
 
