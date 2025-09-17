2e33f7b36adca2f657ef65c120e4158e1e30416f
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-17 23:41:17 +0530

    Q done

--- Practice set/CH-5/Q-6.py
+++ Practice set/CH-5/Q-6.py
@@ -1,5 +1,7 @@
  # Create an empty  dictionary. Allow 4 friends to enter their favourite language as value and use thier name as key.
 
+
+
 d={}
 
 F=input("Enter Friends's name:")
@@ -22,3 +24,5 @@ L=input("Enter the lanugage name:")
 
 d.update({F:L})
 print(d)
+
+
