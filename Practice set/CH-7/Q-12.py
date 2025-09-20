b0db1db091bd53cfd3b4c7fc059423e3edafd341
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-20 23:24:04 +0530

    Q done

--- Practice set/CH-7/Q-12.py
+++ Practice set/CH-7/Q-12.py
@@ -1,7 +1,11 @@
 #  write a program to greet all the person names stored in the list and which starts with S.
 
+
+
 l = ["Harry" , "Shreya" , "Sneha" , "Priya" , "Mohini" ]
 for name in l:
     if(name.startswith("S")):
         print(f"Hello{name}")
+
+        
         
