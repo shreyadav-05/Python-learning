9e66f298c2088549cafbe1784132a13e450c3af9
Shreya Yadav <shreyadavagr42@gmail.com>
2025-09-20 23:18:07 +0530

    Q done

--- Practice set/CH-7/Q-7.py
+++ Practice set/CH-7/Q-7.py
@@ -1,8 +1,12 @@
 #count and print how many numbers between 1 to 50 are divisible by 7.
 
+
+
 count = 0
 for i in range (1 , 51):
     if i % 7 == 0:
         print(i)
         count=count+1
         print("total:" , count )
+
+        
