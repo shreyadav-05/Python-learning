117cfd3c06a8d237b69e5ea019794c8e3747f650
Shreya Yadav <shreyadavagr42@gmail.com>
2025-10-30 23:07:42 +0530

    done

--- Practice set/CH-11/Q-5.py
+++ Practice set/CH-11/Q-5.py
@@ -9,7 +9,7 @@ class complex:
         return complex(self.r + c2.r, self.i + c2.i)
     
     def __str__(self):
-        return f"{self.r} * {self.i}i"
+        return f"{self.r} + {self.i}i"
 
 c1 = complex(1, 2)
 c2 = complex(3, 4)
