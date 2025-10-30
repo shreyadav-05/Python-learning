d3528a1b1476c9bf5c8f6ce411dda216b976219f
Shreya Yadav <shreyadavagr42@gmail.com>
2025-10-30 21:43:37 +0530

    done

--- Practice set/CH-11/Q-4.py
+++ Practice set/CH-11/Q-4.py
@@ -8,8 +8,12 @@ class Employee:
     def salaryAfterIncrement(self):
         return (self.salary + self.salary * (self.increment/100))
     
-
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        self.increment = ((salary/self.salary) -1)*100
 
 
 e = Employee()
 print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 280.8
+print(e.increment)
