e4a9ea58a29a5ea2652fbe139dd3a9e701230641
Shreya Yadav <shreyadavagr42@gmail.com>
2025-11-10 23:17:27 +0530

    done

--- Practice set/CH-9/Q-8.py
+++ Practice set/CH-9/Q-8.py
@@ -1,5 +1,7 @@
 
 # Write a python program to read the content of a file "poem.txt" and check if the word is present in the file or not.
+
+
 f = open("poem.txt")
 content = f.read()
 if("step" in content):
