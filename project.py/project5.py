5af55d18d7b44d15a71f352a02c5f6f88ff6e045
Shreya Yadav <shreyadavagr42@gmail.com>
2025-11-11 21:49:31 +0530

    done

--- project.py/project5.py
+++ project.py/project5.py
@@ -1,4 +1,4 @@
-
+# Print_table(num)
 
 def print_table(num):                     # function define upar
     for i in range(1, 11):                # 1 to 10 loop
@@ -15,5 +15,5 @@ while play == "yes":                      # jab tak user chahe
         print("⚠️ Please enter a valid number!")
         continue                            # loop ko dobara se start karega
     play = input("Do you want to play again? (yes/no): ").lower()
-    
+
 print("thank u for playing")
