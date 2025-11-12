57cef25c34d182deb060928b4b84d22b804e0d00
Shreya Yadav <shreyadavagr42@gmail.com>
2025-11-12 23:48:38 +0530

    done

--- Practice set/CH-11/Q-5.py
+++ Practice set/CH-11/Q-5.py
@@ -1,5 +1,7 @@
 #Write a class Complex to represent complex numbers, along with overloaded operators + and * which add and multiply them.
 
+
+
 class complex:
     def __init__(self, r, i):
         self.r = r
@@ -15,3 +17,5 @@ c1 = complex(1, 2)
 c2 = complex(3, 4)
 print(c1 + c2)
     
+
+
