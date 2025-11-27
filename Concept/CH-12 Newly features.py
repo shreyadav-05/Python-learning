cae2b4993a69c67032c769d44865697c51e2ab55
Shreya Yadav <shreyadavagr42@gmail.com>
2025-11-27 16:37:13 +0530

    done

--- Concept/CH-12 Newly features.py
+++ Concept/CH-12 Newly features.py
@@ -166,3 +166,15 @@ finally:
 
 
 #finally use
+def main():   
+    try:
+        a = int(input("Hey, Enter a number: "))
+        print(a)
+        return
+    
+    except Exception as e:
+        print(e)
+        return
+    
+    finally:
+        print("I am inside of finally")
