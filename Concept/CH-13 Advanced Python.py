c474532d7dceaacbf41427ad99669891861094b5
Shreya Yadav <shreyadavagr42@gmail.com>
2025-12-24 16:14:47 +0530

    done

--- Concept/CH-13 Advanced Python.py
+++ Concept/CH-13 Advanced Python.py
@@ -72,3 +72,11 @@ print(result)
 nums = [1, 2, 3, 4, 5]
 result = list(filter(lambda x: x % 2 == 0, nums))
 print(result)
+
+#reduce() (from functools)Sab values ko combine karta hai
+from functools import reduce
+
+nums = [1, 2, 3, 4]
+result = reduce(lambda a, b: a + b, nums)
+print(result)
+
